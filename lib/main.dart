3caa76220cb420baa9ae77ce5f5e355074ef71c7
elena-atanasoska <elena.atanasoska@students.finki.ukim.mk>
2024-02-08 22:41:27 +0100

    edit and delete events

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,9 @@ class _MyHomePageState extends State<MyHomePage> {
         view: CalendarView.day,
         dataSource: events,
         onTap: (CalendarTapDetails details) {
-          if (details.targetElement == CalendarElement.calendarCell) {
+          if (details.appointments != null && details.appointments!.isNotEmpty) {
+            _showEventDetailsDialog(details.appointments![0]);
+          } else if (details.targetElement == CalendarElement.calendarCell) {
             _showAddEventDialog(details.date!);
           }
         },
@@ -125,6 +127,114 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  Future<void> _showEventDetailsDialog(Appointment event) async {
+    TextEditingController titleController = TextEditingController(text: event.subject);
+    TextEditingController dateController = TextEditingController(text: formatDate(event.startTime));
+    TextEditingController timeController = TextEditingController(text: formatTime(event.startTime));
+    TextEditingController durationController = TextEditingController(text: (event.endTime.difference(event.startTime).inHours).toString());
+
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Event Details'),
+          content: SingleChildScrollView(
+            child: IntrinsicHeight(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextField(
+                    controller: titleController,
+                    decoration: InputDecoration(
+                      labelText: 'Event Title',
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  TextField(
+                    controller: dateController,
+                    decoration: InputDecoration(labelText: 'Date (YYYY-MM-DD)'),
+                  ),
+                  SizedBox(height: 10.0),
+                  TextField(
+                    controller: timeController,
+                    decoration: InputDecoration(labelText: 'Time (HH:MM)'),
+                  ),
+                  SizedBox(height: 10.0),
+                  TextField(
+                    controller: durationController,
+                    decoration: InputDecoration(labelText: 'Duration (hours)'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          actions: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      _deleteEvent(event);
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Delete'),
+                  ),
+                  SizedBox(width: 5.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      _editEvent(
+                        event,
+                        titleController.text,
+                        dateController.text,
+                        timeController.text,
+                        durationController.text,
+                      );
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Save'),
+                  ),
+                  SizedBox(width: 5.0)
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+
+  void _editEvent(
+      Appointment event,
+      String title,
+      String date,
+      String time,
+      String duration,
+      ) {
+    setState(() {
+      final DateTime newDateTime = DateTime.parse('$date $time');
+      final DateTime startTime = DateTime(
+        newDateTime.year,
+        newDateTime.month,
+        newDateTime.day,
+        newDateTime.hour,
+        newDateTime.minute,
+      );
+
+      // Calculate the duration dynamically
+      final DateTime endTime = startTime.add(Duration(hours: int.parse(duration)));
+
+      // Update the existing event details
+      event.subject = title;
+      event.startTime = startTime;
+      event.endTime = endTime;
+
+      events = MeetingDataSource(appointments);
+    });
+  }
+
   void _addEvent(
       DateTime selectedDate,
       String title,
@@ -151,6 +261,13 @@ class _MyHomePageState extends State<MyHomePage> {
           color: Colors.blue,
         ),
       );
+      events = MeetingDataSource(appointments);
+    });
+  }
+
+  void _deleteEvent(Appointment event) {
+    setState(() {
+      appointments.remove(event);
       events = MeetingDataSource(appointments);
     });
   }
